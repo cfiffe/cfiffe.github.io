f0353e9f5cf7630e96cce67d77ff60fc9a3b3b36
cfiffe <55287647+cfiffe@users.noreply.github.com>
2020-01-15 11:32:04 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -1,2 +1,20 @@
 
 document.write("hello world");
+
+document.write("Using Math.sqrt <br/>")
+for(var k = 0; k < 9; k++)
+  {
+    var ans = Math.sqrt(k);
+    document.write(ans + "<br/>");
+  }
+
+document.write("<br/>  Using Math.random")
+
+var beginning = 40;
+
+while(beginning > 0)
+   {
+    document.write(" Current Count: " + beginning + "<br />")
+    beginning = Math.random () * 11;
+     beginning--;
+  }
