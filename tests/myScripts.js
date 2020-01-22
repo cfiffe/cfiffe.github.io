6a942e3ad6548cce4425eedb1878b850d014bf85
cfiffe <55287647+cfiffe@users.noreply.github.com>
2020-01-22 11:27:45 -0500

    Update myScripts.js

--- tests/myScripts.js
+++ tests/myScripts.js
@@ -17,4 +17,18 @@ while(beginning > 0)
     document.write(" Current Count: " + beginning + "<br />")
     beginning = Math.random () * 11;
      beginning--;
-  }
+     
+     
+ // new input loop 1-22-20
+ function myLoop() {
+  
+var start = document.getElementById("start").value;
+var end = document.getElementById("end").value;
+
+for(var i = start; i<= end; i++)
+ {
+   document.write('<p>' + i);
+ } 
+}
+   
+   }
